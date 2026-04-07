106bf37ccab429bd5ba0d791db63c3f80bc3f196
Alisson_PersonalNot <alissoncavalcanticma@gmail.com>
2026-04-07 06:46:05 -0300

    Include migrations of insert Categorias and Produtos

---
+++ APICatalogo/Migrations/20260407090936_PopulaProdutos.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Produtos (Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) VALUES ('Coca-Cola', 'Regrigerante de Cola 350 ml', 5.45, 'cocacola.jpg', 50, now(), 1)");
+            mb.Sql("INSERT INTO Produtos (Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) VALUES ('Salgado', 'Coxinha de frango', 8.50, 'salgado.jpg', 10, now(), 2)");
+            mb.Sql("INSERT INTO Produtos (Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) VALUES ('Pudim 100g', 'Pudim de Leite 100g', 6.75, 'pudim.jpg', 20, now(), 3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM Produtos");
+        }
+    }
+}
