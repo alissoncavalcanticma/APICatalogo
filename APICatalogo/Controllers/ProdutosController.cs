f2897b9cd98329f9683bfbe0a2829b397331e557
Alisson_PersonalNot <alissoncavalcanticma@gmail.com>
2026-04-07 08:12:02 -0300

    Create Controller Produtos

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,16 @@
+﻿using APICatalogo.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context) {
+            _context = context;
+        }
+    }
+}
