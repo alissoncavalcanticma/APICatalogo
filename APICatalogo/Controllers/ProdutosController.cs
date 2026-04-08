fa3050504b8f0e5624d734cfd85966d31150ffb4
Alisson_PersonalNot <alissoncavalcanticma@gmail.com>
2026-04-08 08:12:01 -0300

    create put method

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -1,6 +1,7 @@
 ﻿using APICatalogo.Context;
 using APICatalogo.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace APICatalogo.Controllers
 {
@@ -21,11 +22,32 @@ namespace APICatalogo.Controllers
             return produtos;
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name="ObterProduto")]
         public ActionResult<Produto> Get(int id) {
             var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
             if (produto is null) return NotFound("Produto não encontrado...");
             return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Produto produto) {
+
+            if (produto is null) return BadRequest("Produto inválido!");
+
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Produto produto) {
+            if (id != produto.ProdutoId) return BadRequest("Id não correspondente.");
+
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(produto);
         }
     }
 }
