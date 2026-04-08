684ff5bf654b3327b95f4ea23745691a2ba7e1ac
Alisson_PersonalNot <alissoncavalcanticma@gmail.com>
2026-04-08 07:25:24 -0300

    Create GET methods

---
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,17 @@
+﻿using APICatalogo.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class CategoriasController
+    {
+        public readonly AppDbContext _context;
+
+        public CategoriasController(AppDbContext context) {
+            _context = context;
+        }
+    }
+}
